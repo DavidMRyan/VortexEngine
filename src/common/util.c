373a4b3e7c7c4eda544b337613709b5c485eebd1
DavidMRyan <davidryan0119@gmail.com>
2023-06-24 23:10:15 -0400

    Fixed Segmentation Fault in Read_Shader function

--- src/common/util.c
+++ src/common/util.c
@@ -4,46 +4,35 @@
 
 #include "../include/util.h"
 
-char* read_shader(const char* p_infile_name) {
-    FILE* p_infile = fopen(p_infile_name, "r");
-    char* final = NULL;
-    size_t final_size = 0;
-
-    if (p_infile != NULL) {
-        char line[255];
-        while (fgets(line, sizeof(line), p_infile)) {
-            // Calculate the new size required for 'final'
-            size_t line_length = strlen(line);
-            size_t new_size = final_size + line_length + 1;
-
-            // Reallocate memory for 'final' to accommodate the new line
-            char* new_final = realloc(final, new_size);
-            if (new_final == NULL) {
-                perror("Memory allocation error");
-                free(final); // Free the previously allocated memory
-                fclose(p_infile);
-                return NULL;
-            }
-
-            final = new_final;
-            strcat(final, line); // Concatenate 'line' to 'final'
-            final_size = new_size;
-        }
-
-        fclose(p_infile);
-
-        // Remove the first character from 'final'
-        if (final_size > 1) {
-            memmove(final, final + 1, final_size - 1);
-            final = realloc(final, final_size);  // Resize the string to remove the extra character
-        } else {
-            // The string is empty
-            *final = '\0';
-        }
-
-        return final;
-    } else {
-        perror("Error opening input file");
+char* read_shader(const char* filename) {
+    FILE* file = fopen(filename, "r");
+    if (file == NULL) {
+        fprintf(stderr, "Failed to open file: %s\n", filename);
         return NULL;
     }
+
+    char* buffer = NULL;
+    char* temp = NULL;
+    size_t buffer_size = 0;
+    size_t line_length;
+    char line[128];  // Adjust the buffer size as needed
+
+    while (fgets(line, sizeof(line), file) != NULL) {
+        line_length = strlen(line);
+        temp = realloc(buffer, buffer_size + line_length + 1);
+
+        if (temp == NULL) {
+            fclose(file);
+            free(buffer);
+            fprintf(stderr, "Failed to allocate memory.\n");
+            return NULL;
+        }
+
+        buffer = temp;
+        strcpy(buffer + buffer_size, line);
+        buffer_size += line_length;
+    }
+
+    fclose(file);
+    return buffer;
 }
