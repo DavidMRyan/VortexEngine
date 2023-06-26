7f34c8c23b1b333496ab200a048539feaa00e6fb
DavidMRyan <davidryan0119@gmail.com>
2023-06-25 20:20:27 -0400

    Indexed Draws Testing

--- main.c
+++ main.c
@@ -5,22 +5,81 @@
 #include <GL/glew.h>
 #include <GL/freeglut.h>
 #include <math.h>
+#include <process.h>
 
 #include "src/include/util.h"
 #include "src/include/math_3d.h"
 
 GLuint VBO;
+GLuint IBO;
 GLint U_TRANSFORM;
+GLint U_WORLDPOS;
 
 static void create_vertex_buffer() {
-    Vector3f Vertices[3];
-    Vertices[0] = new_vec3f(-1.0f, -1.0f, 0.0f);    // Bottom left
-    Vertices[1] = new_vec3f(1.0f, -1.0f, 0.0f);     // Bottom right
-    Vertices[2] = new_vec3f(0.0f, 1.0f, 0.0f);      // Top
+    Vertex vertices[19];
+
+    // Center
+    vertices[0] = new_vertex(0.0f, 0.0f);
+
+    // Top Row
+    vertices[1] = new_vertex(-1.0f, 1.0f);
+    vertices[2] = new_vertex(-0.75f, 1.0f);
+    vertices[3] = new_vertex(-0.50f, 1.0f);
+    vertices[4] = new_vertex(-0.25f, 1.0f);
+    vertices[5] = new_vertex(-0.0f, 1.0f);
+    vertices[6] = new_vertex(0.25f, 1.0f);
+    vertices[7] = new_vertex(0.50f, 1.0f);
+    vertices[8] = new_vertex(0.75f, 1.0f);
+    vertices[9] = new_vertex(1.0f, 1.0f);
+
+    // Bottom Row
+    vertices[10] = new_vertex(-1.0f, -1.0f);
+    vertices[11] = new_vertex(-0.75f, -1.0f);
+    vertices[12] = new_vertex(-0.50f, -1.0f);
+    vertices[13] = new_vertex(-0.25f, -1.0f);
+    vertices[14] = new_vertex(-0.0f, -1.0f);
+    vertices[15] = new_vertex(0.25f, -1.0f);
+    vertices[16] = new_vertex(0.50f, -1.0f);
+    vertices[17] = new_vertex(0.75f, -1.0f);
+    vertices[18] = new_vertex(1.0f, -1.0f);
 
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+}
+
+static void create_index_buffer() {
+    unsigned int indices[] = {
+            // Top Tris
+            0, 2, 1,
+            0, 3, 2,
+            0, 4, 3,
+            0, 5, 4,
+            0, 6, 5,
+            0, 7, 6,
+            0, 8, 7,
+            0, 9, 8,
+
+            // Bottom Tris
+            0, 10, 11,
+            0, 11, 12,
+            0, 12, 13,
+            0, 13, 14,
+            0, 14, 15,
+            0, 15, 16,
+            0, 16, 17,
+            0, 17, 18,
+
+            // Left Tri
+            0, 1, 10,
+
+            // Right Tri
+            0, 18, 9
+    };
+
+    glGenBuffers(1, &IBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 }
 
 static void add_shader(GLuint shader_program, const char* p_shader_data, GLenum shader_type) {
@@ -94,12 +153,12 @@ static void compile_shaders() {
     // Uniform Testing
 
     // Universal Transform Modifier
-    U_TRANSFORM = glGetUniformLocation(shader_program, "transform");
-
-    if(U_TRANSFORM == -1) {
-        printf("Error getting uniform 'scale' in Vertex Shader.\n");
-        exit(1);
-    }
+//    U_TRANSFORM = glGetUniformLocation(shader_program, "transform");
+//
+//    if(U_TRANSFORM == -1) {
+//        printf("Error getting uniform 'scale' in Vertex Shader.\n");
+//        exit(1);
+//    }
 
     // -----------------------------------
 
@@ -116,27 +175,6 @@ static void compile_shaders() {
 }
 
 void transform_test() {
-    static float scale = 1.0f;
-    static float delta = 0.00025f;
-    scale += delta;
-
-    if(scale >= 1.5f || scale <= 0.5f)
-        delta *= -1.0f;
-
-    Matrix4f scale_transform = new_matrix4f(scale, 0.0f, 0.0f, 0.0f,    // X
-                                        0.0f, scale, 0.0f, 0.0f,        // Y
-                                        0.0f, 0.0f, scale, 0.0f,        // Z
-                                        0.0f, 0.0f, 0.0f, 1.0f);        // W
-    glUniformMatrix4fv(U_TRANSFORM, 1, GL_TRUE, &scale_transform.mat[0][0]);
-}
-
-void draw() {
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    // -----------------------------------
-    // Uniform Tests
-
-
     // -----------------------------------
     // Scale
 
@@ -192,27 +230,52 @@ void draw() {
 //    glUniformMatrix4fv(U_TRANSFORM, 1, GL_TRUE, &rotation.mat[0][0]);
 
     // -----------------------------------
+}
 
+void draw() {
+    glClear(GL_COLOR_BUFFER_BIT);
 
-    // -----------------------------------
-    // Combination Test
+    // Transformation Testing
+    // transform_test();
 
-    transform_test();
+    // -----------------------------------------
+    // Indexed Draws Testing
 
-    // END Uniform Tests
-    // -----------------------------------
+    static float scale = 0.0f;
+//    scale += 0.0001f;
+
+    Matrix4f world = new_matrix4f(cosf(scale), -sinf(scale), 0.0f, 0.0f,
+                                  sinf(scale), cosf(scale), 0.0f, 0.0f,
+                                  0.0f, 0.0f, 1.0f, 0.0f,
+                                  0.0f, 0.0f, 0.0f, 1.0f);
+    glUniformMatrix4fv(U_WORLDPOS, 1, GL_TRUE, &world.mat[0][0]);
+
+    // -----------------------------------------
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
-    glDisableVertexAttribArray(0);
-    glutPostRedisplay();
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
 
+    // Position (Vertex->pos)
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), 0);
+
+    // Color (Vertex->col)
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+
+
+    glDrawElements(GL_TRIANGLES, 54, GL_UNSIGNED_INT, 0);
+    glDisableVertexAttribArray(0);
+    glDisableVertexAttribArray(1);
+
+    glutPostRedisplay();
     glutSwapBuffers();
 }
 
 int main(int argc, char** argv) {
+    // Random seed based on PID
+    srand(getpid());
+
     // Create GLUT Handle & Initialize Properties
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
@@ -231,11 +294,12 @@ int main(int argc, char** argv) {
     GLclampf r = 0.0f, g = 0.0f, b = 0.0f, a = 0.0f;
     glClearColor(r, g, b, a);
 
-    // Create Vertex Buffer & Compile Shader Program
+    // Create Vertex Buffer, Index Buffer & Compile Shader Program
     create_vertex_buffer();
+    create_index_buffer();
     compile_shaders();
-    glutDisplayFunc(draw);
 
+    glutDisplayFunc(draw);
     glutMainLoop();
     return 0;
 }
